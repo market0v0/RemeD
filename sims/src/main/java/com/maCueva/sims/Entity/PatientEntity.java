d1199ef0e04e59c6d1b37c218119960546666b2a
Charmaine Mae Ramirez <charmainemae.ramirez@cit.edu>
2022-11-15 10:31:37 +0800

    Additional Patient Entity Data Members(Also for trial commit of Ramirez)

--- sims/src/main/java/com/maCueva/sims/Entity/PatientEntity.java
+++ sims/src/main/java/com/maCueva/sims/Entity/PatientEntity.java
@@ -9,20 +9,37 @@ public class PatientEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    
     @Column(unique=true)
     private String username;
     private String password;
+    private String firstname;
+	private String middlename;
+	private String lastname;
+	private int weight;
+	private int height;
+
+
+	public PatientEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 
 
-    public PatientEntity() {
-    }
+	public PatientEntity(int id, String username, String password, String firstname, String middlename, String lastname,
+			int weight, int height) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.middlename = middlename;
+		this.lastname = lastname;
+		this.weight = weight;
+		this.height = height;
+	}
 
-    public PatientEntity(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
 
     public int getId() {
         return id;
@@ -44,4 +61,63 @@ public class PatientEntity {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    public String getFirstname() {
+		return firstname;
+	}
+
+
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+
+
+	public String getMiddlename() {
+		return middlename;
+	}
+
+
+
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+
+	public int getWeight() {
+		return weight;
+	}
+
+
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
 }
