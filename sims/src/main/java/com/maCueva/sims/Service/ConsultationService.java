083c073d940b6a88b299ed2a0a7536df01099943
Charmaine Mae Ramirez <113568944+charamirez05@users.noreply.github.com>
2022-11-22 11:01:32 +0800

    Ramirez Entities Files

---
+++ sims/src/main/java/com/maCueva/sims/Service/ConsultationService.java
@@ -0,0 +1,65 @@
+package com.ramirezremed.RemeD.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramirezremed.RemeD.Entity.ConsultationEntity;
+import com.ramirezremed.RemeD.Repository.ConsultationRepository;
+
+
+@Service
+public class ConsultationService {
+	
+	@Autowired
+	ConsultationRepository crepo;
+
+	
+		public ConsultationEntity createConsultation(ConsultationEntity consultation) {
+			return crepo.save(consultation);
+		}
+		
+		
+		public List <ConsultationEntity> getConsultation(){
+			return crepo.findAll();
+		}
+		
+		
+
+		public ConsultationEntity putConsultation(int consultationid, ConsultationEntity newConsultationDetails) throws Exception{
+			
+			ConsultationEntity consultation = new ConsultationEntity();
+			
+			try {
+				consultation = crepo.findById(consultationid).get(); 
+
+				consultation.setConsultationPurpose(newConsultationDetails.getConsultationPurpose());
+				
+				return crepo.save(consultation);
+			}
+			
+			catch(NoSuchElementException ex){
+				throw new Exception("Consultation ID" + consultationid + "does not exist.");	
+			}	
+		}
+		
+		
+		public String deleteConsultation(int consultationid) { 
+			String msg;
+			
+			if(crepo.findById(consultationid) !=  null) {	
+				crepo.deleteById(consultationid); 		
+			
+				msg = "Consultation ID Number " + consultationid + "successfully deleted.";
+			}
+			
+			else 
+				msg = "Consultation ID Number" + consultationid + " not found";
+			
+			return msg;
+		}
+
+
+}
---
+++ sims/src/main/java/com/maCueva/sims/Service/LaboratoryTestService.java
@@ -0,0 +1,62 @@
+package com.ramirezremed.RemeD.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramirezremed.RemeD.Entity.LaboratoryTestEntity;
+import com.ramirezremed.RemeD.Repository.LaboratoryTestRepository;
+
+@Service
+public class LaboratoryTestService {
+	
+	@Autowired
+	LaboratoryTestRepository ltrepo;
+
+	
+		public LaboratoryTestEntity createLaboratoryTest(LaboratoryTestEntity labtest) {
+			return ltrepo.save(labtest);
+		}
+		
+		
+		public List <LaboratoryTestEntity> getAllLaboratoryTest(){
+			return ltrepo.findAll();
+		}
+		
+		
+
+		public LaboratoryTestEntity putLaboratory(int labtestid, LaboratoryTestEntity newLabTestDetails) throws Exception{
+			
+			LaboratoryTestEntity labtest = new LaboratoryTestEntity();
+			
+			try {
+				labtest = ltrepo.findById(labtestid).get(); 
+
+				labtest.setLabtesttype(newLabTestDetails.getLabtesttype());
+				
+				return ltrepo.save(labtest);
+			}
+			
+			catch(NoSuchElementException ex){
+				throw new Exception("Laboratory Test ID" + labtestid + " does not exist.");	
+			}	
+		}
+		
+		
+		public String deleteLaboratoryTest(int labtestid) { 
+			String msg;
+			
+			if(ltrepo.findById(labtestid) !=  null) {	
+				ltrepo.deleteById(labtestid); 		
+			
+				msg = "Laboratory Test ID Number " + labtestid + " successfully deleted.";
+			}
+			
+			else 
+				msg = "Laboratory Test ID Number" + labtestid + " not found";
+			
+			return msg;
+		}
+}
