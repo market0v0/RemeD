2f233a6255700922f2d82b2ee4eecd624f8dcf2b
Charmaine Mae Ramirez <charmainemae.ramirez@cit.edu>
2022-11-24 12:18:03 +0800

    Result and Record Mapping (many to one)
    Result and Labtest Mapping (one to one)

--- sims/src/main/java/com/maCueva/sims/Entity/ResultEntity.java
+++ sims/src/main/java/com/maCueva/sims/Entity/ResultEntity.java
@@ -1,8 +1,12 @@
 package com.maCueva.sims.Entity;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,35 +19,76 @@ public class ResultEntity {
 	private String resultdate;
 	private String resultstatus;
 	
+	@OneToOne(cascade = CascadeType.MERGE)
+	private LaboratoryTestEntity labtest;
+	
+	
+	@ManyToOne
+	@JoinColumn(name="recordid")
+	RecordEntity record;
+	
+	
 	public ResultEntity () {}
-	public ResultEntity(int resultid, String resultdate, String resultstatus) {
+	
+	public ResultEntity(int resultid, String resultdate, String resultstatus, LaboratoryTestEntity labtest,
+			RecordEntity record) {
 		super();
 		this.resultid = resultid;
 		this.resultdate = resultdate;
 		this.resultstatus = resultstatus;
+		this.labtest = labtest;
+		this.record = record;
 	}
+
+
+
+
 	@Override
 	public String toString() {
 		return "ResultEntity [resultid=" + resultid + ", resultdate=" + resultdate + ", resultstatus=" + resultstatus
 				+ "]";
 	}
+	
 	public int getResultid() {
 		return resultid;
 	}
+	
 	public void setResultid(int resultid) {
 		this.resultid = resultid;
 	}
+	
 	public String getResultdate() {
 		return resultdate;
 	}
+	
 	public void setResultdate(String resultdate) {
 		this.resultdate = resultdate;
 	}
+	
 	public String getResultstatus() {
 		return resultstatus;
 	}
+	
 	public void setResultstatus(String resultstatus) {
 		this.resultstatus = resultstatus;
 	}
+
+	public LaboratoryTestEntity getLabtest() {
+		return labtest;
+	}
+
+	public void setLabtest(LaboratoryTestEntity labtest) {
+		this.labtest = labtest;
+	}
+
+	public RecordEntity getRecord() {
+		return record;
+	}
+
+	public void setRecord(RecordEntity record) {
+		this.record = record;
+	}
+	
+	
 	
 }
